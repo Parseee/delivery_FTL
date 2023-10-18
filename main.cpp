300a65e2996dd16d514f968c3e67b64368438d20
Parseee <babakovi679@gmail.com>
2023-10-18 10:53:23 +0300

    message: "test"

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <vector>
 #include <random>
-#include "request.h"
-#include "courier.h"
+//#include "request.h"
+//#include "courier.h"
 
 int main() {
     // some code for gui
-
+/*
     while (true) {
         // add random time for waiting
         Request *request = new Request();
         Dispatcher dispatcher(request);
     }
+    */
+   std::cout << "ibjuf\n";
 }
