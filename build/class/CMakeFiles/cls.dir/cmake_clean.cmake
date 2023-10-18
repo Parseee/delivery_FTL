e18b64c98f10826416d284b027c05b5b59845add
Parseee <babakovi679@gmail.com>
2023-10-18 10:55:41 +0300

    message: "iuen"

---
+++ build/class/CMakeFiles/cls.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cls.dir/courier.cpp.o"
+  "CMakeFiles/cls.dir/courier.cpp.o.d"
+  "libcls.a"
+  "libcls.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cls.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
