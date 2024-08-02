0043473a6427a8063145771a970604f0a2eb70b4
Parseee <81364541+Parseee@users.noreply.github.com>
2024-08-02 21:51:02 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ilababakov/Documents/code/study_solutions/prac/delivery_FTL
-# Build directory: /Users/ilababakov/Documents/code/study_solutions/prac/delivery_FTL/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
